63527c24243f0f4553813e915ec6002b03527fc9
andrezza5 <andrezzamarcilio@gmail.com>
2023-05-15 17:21:05 -0300

    atual

--- src/components/ItemPortfolio/ItemPortfolio.jsx
+++ src/components/ItemPortfolio/ItemPortfolio.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import "./ItemPortfolio.css";
-import imagemPortfolio from "../../assets/img/ftsitedesbrave.png";
+
 
 function ItemPortfolio(props) {
     return (
     <div className="item-portfolio">
             <a href={props.link} target="_blank">
-            <img src={imagemPortfolio} alt="Portfolio"/>   
+            <img src={props.image} alt="Portfolio"/>   
             <p>{props.title}</p>
             </a>
         </div>  
