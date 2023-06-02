301836990522ea26d9170e21c0b1d6b9ecaa0d01
andrezza5 <andrezzamarcilio@gmail.com>
2023-06-02 09:30:41 -0300

    atualizacao

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://server-a-xhpr.onrender.com",
+    baseURL: "https://site-pessoal-api-1f0o.onrender.com/api",
 });
 
 export default api;
